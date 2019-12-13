e318a1084d9fcfff501212f23cf4294ea3ee5624
Virllinia <virlli_nia@hotmail.com>
2019-12-13 20:38:59 +0100

    🖌 Fonts

---
+++ lib/ui/pages/payment_method_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PaymentMethodPage extends StatefulWidget {
+  PaymentMethodPage({Key key}) : super(key: key);
+
+  @override
+  _PaymentMethodPageState createState() => _PaymentMethodPageState();
+}
+
+class _PaymentMethodPageState extends State<PaymentMethodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
